89ff00e8fcc3e54adfbaf4106c4d48969cd7a277
Norman Lie <norman.lie@kudo.co.id>
2018-04-04 14:58:49 +0700

    create home activity

---
+++ app/src/main/java/com/arcanum/arcanumstoremanager/feature/home/HomeActivity.java
@@ -0,0 +1,50 @@
+package com.arcanum.arcanumstoremanager.feature.home;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.arcanum.arcanumstoremanager.R;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_home)
+public class HomeActivity extends AppCompatActivity {
+
+    @ViewById(R.id.message)
+    TextView mTextMessage;
+
+    @ViewById(R.id.navigation)
+    BottomNavigationView navigation;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @AfterViews
+    public void initAfterViews() {
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}

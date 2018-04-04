f5a0280729e1104435383ccee375421a71107c08
Norman Lie <norman.lie@kudo.co.id>
2018-04-04 09:44:22 +0700

    add new exception used in add product validation

---
+++ app/src/main/java/com/arcanum/arcanumstoremanager/exception/EmptyNameException.java
@@ -0,0 +1,11 @@
+package com.arcanum.arcanumstoremanager.exception;
+
+/**
+ * Created by norman on 04/04/18.
+ */
+
+public class EmptyNameException extends Exception {
+    public EmptyNameException(String message) {
+        super(message);
+    }
+}
---
+++ app/src/main/java/com/arcanum/arcanumstoremanager/exception/EmptyProductCodeException.java
@@ -0,0 +1,11 @@
+package com.arcanum.arcanumstoremanager.exception;
+
+/**
+ * Created by norman on 04/04/18.
+ */
+
+public class EmptyProductCodeException extends Exception {
+    public EmptyProductCodeException(String message) {
+        super(message);
+    }
+}
